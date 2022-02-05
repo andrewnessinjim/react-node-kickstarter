22537406f98baf624961594614af10c90e1bcd16
Andrew Nessin <andrewnessinjim@yahoo.com>
2022-02-05 12:50:09 +0530

    Correct test case name

--- e2e/cypress/integration/1-getting-started/spec.js
+++ e2e/cypress/integration/1-getting-started/spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress"/>
 
-it('displays hello world', () => {
+it('displays create-react-app\'s default text', () => {
   cy.visit('/');
   cy.get('.App-header p')
       .should('contain', 'Edit src/App.tsx and save to reload.');
